c82630404dca934f801aee11e3d2168e3e57fa17
RonaldOrellana <to0339032025@unab.edu.sv>
2025-11-08 19:57:56 -0600

    se agregan nuevos cambios

--- clinica/models.py
+++ clinica/models.py
@@ -28,14 +28,13 @@ class Medico(models.Model):
 
 # Modelo Servicio
 class Servicio(models.Model):
-    medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name="servicios", null=True, blank=True)
+    medico = models.ForeignKey(Medico, on_delete=models.CASCADE, related_name="servicios")
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     precio = models.DecimalField(max_digits=8, decimal_places=2)
 
     def __str__(self):
-        return self.nombre
-
+        return f"{self.nombre} ({self.medico.nombre} {self.medico.apellido})"
 
 # Modelo Cita
 class Cita(models.Model):
