749bbef8eb98e464ad9547329f403563ee193fc3
RonaldOrellana <to0339032025@unab.edu.sv>
2025-11-11 12:31:08 -0600

    se agregan nuevos cambios nuevos

--- clinica/views.py
+++ clinica/views.py
@@ -542,3 +542,13 @@ def registrar_cita(request):
         "servicios_otros": servicios_otros,
         "selected_servicio": None
     })
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Perfil
+
+@receiver(post_save, sender=User)
+def crear_perfil(sender, instance, created, **kwargs):
+    if created:
+        Perfil.objects.create(user=instance)
